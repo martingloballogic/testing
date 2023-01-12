6a35d4d938e5efe2b2178ecc3e35efafe4226d78
martingloballogic <104190696+martingloballogic@users.noreply.github.com>
2023-01-12 22:02:07 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class sample{
     public static void main(String[] args){
-        System.out.println("Hello, World!"); 
+        System.out.println("This is the change what i was looking for sometime"); 
     }
 }
